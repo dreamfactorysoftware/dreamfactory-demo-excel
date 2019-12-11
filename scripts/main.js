5508c528539c458235616c36ad7d87ceb186d8aa
Oleksandr Onoshko <oleksandron@softwareplanet.group>
2019-12-11 16:17:16 +0200

    DP-120 Create Excel demo project
    - Implement saving jwt in LocalStorage
    - Implement get user by id from df
    - Implement displaying username in navbar
    - Implement logout button
    - Implement handling when token not valid or user not exist in dreamfactory platform
    - Change login form field from telephone number to email

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,54 @@
 
-let jwt = window.location.href.split('jwt')[1];
+let jwt = window.location.href.split('jwt=')[1];
+
+function redirectToLogin() {
+    window.location.assign('/login');
+}
 
 if (!jwt) {
-    window.location.assign('/login');
+    redirectToLogin();
+}
+else {
+    localStorage.setItem('jwt', jwt);
+    getPayload(jwt);
+}
+
+
+function getPayload(jwt) {
+    try {
+        let payload = JSON.parse(atob(jwt.split('.')[1]));
+        getUserById(payload.user_id);
+    }
+    catch (e) {
+        console.error(e);
+        redirectToLogin();
+    }
+}
+
+function getUserById(id) {
+    fetch(`http://excel.staging-spg.dreamfactory.com/api/v2/system/user/${id}`, {
+        dataType: 'json',
+        headers: {
+            "X-DreamFactory-Api-Key": config.apiKey
+        },
+    })
+        .then(response => response.json())
+        .then(data => {
+            if (!data || !data.username) {
+                redirectToLogin();
+            }
+            else {
+                setUsername(data.username)
+            }
+        })
+        .catch(error => console.error(error));
+}
+
+function setUsername(username) {
+    document.getElementById('username').innerText = username.replace('+okta_sso', '');
+}
+
+function logout() {
+    redirectToLogin();
+    localStorage.removeItem('jwt');
 }
